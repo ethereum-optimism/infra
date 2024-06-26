974652dc9a8b486ededb791ed5d0805531e7cbdd
daniel1302 <daniel.1302@gmail.com>
2024-06-26 21:45:46 +0200

    feat: initial dynamic authentication with postgresql

--- proxyd/proxyd.go
+++ proxyd/proxyd.go
@@ -353,6 +353,7 @@ func Start(config *Config) (*Server, func(), error) {
 		config.Server.MaxRequestBodyLogLen,
 		config.BatchConfig.MaxSize,
 		limiterFactory,
+		config.DynamicAuthentication,
 	)
 	if err != nil {
 		return nil, nil, fmt.Errorf("error creating server: %w", err)
