ca161068676baea2a9e0917f6db58e1475216433
Jacob Elias <jacobelias.gv@gmail.com>
2024-07-11 16:27:08 -0500

    feat: added dozzle to test env

--- proxyd/tools/test-env/start-env.sh
+++ proxyd/tools/test-env/start-env.sh
@@ -3,6 +3,25 @@
 # Source your environment variables
 source .env
 
+# Iterate through all arguments
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -build)
+            echo "-build argument detected"
+            pushd ../../
+            make proxyd
+            cd ..
+            docker buildx build -f ./proxyd/Dockerfile -t ${IMAGE_NAME}:${IMAGE_TAG} . --platform linux/arm64
+            popd
+            ;;
+        *)
+            # Handle other arguments if needed
+            ;;
+    esac
+    shift # Move to the next argument
+done
+
+
 # Prepare the config file
 envsubst < ./proxyd/proxyd/proxyd.toml.template > ./proxyd/proxyd/proxyd.toml
 envsubst < ./proxyd/upstream-proxyd-1/proxyd.toml.template > ./proxyd/upstream-proxyd-1/proxyd.toml
@@ -10,4 +29,4 @@ envsubst < ./proxyd/upstream-proxyd-2/proxyd.toml.template > ./proxyd/upstream-p
 
 
 # Start Docker Compose
-docker-compose up -d
+docker-compose up -d --force-recreate
