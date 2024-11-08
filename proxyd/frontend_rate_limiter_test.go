2dc1de9357fe790e80ceb06e35b64e9f3ae3f033
Julian Meyer <julian.meyer@coinbase.com>
2024-11-08 11:34:11 -0800

    Add test for fallback rate limiter

--- proxyd/frontend_rate_limiter_test.go
+++ proxyd/frontend_rate_limiter_test.go
@@ -52,3 +52,35 @@ func TestFrontendRateLimiter(t *testing.T) {
 		})
 	}
 }
+
+type errorFrontend struct{}
+
+func (e *errorFrontend) Take(ctx context.Context, key string) (bool, error) {
+	return false, fmt.Errorf("test error")
+}
+
+var _ FrontendRateLimiter = &errorFrontend{}
+
+func TestFallbackRateLimiter(t *testing.T) {
+	shouldSucceed := []FrontendRateLimiter{
+		NewFallbackRateLimiter(NoopFrontendRateLimiter, NoopFrontendRateLimiter),
+		NewFallbackRateLimiter(NoopFrontendRateLimiter, &errorFrontend{}),
+		NewFallbackRateLimiter(&errorFrontend{}, NoopFrontendRateLimiter),
+	}
+
+	shouldFail := []FrontendRateLimiter{
+		NewFallbackRateLimiter(&errorFrontend{}, &errorFrontend{}),
+	}
+
+	ctx := context.Background()
+	for _, frl := range shouldSucceed {
+		ok, err := frl.Take(ctx, "foo")
+		require.NoError(t, err)
+		require.True(t, ok)
+	}
+	for _, frl := range shouldFail {
+		ok, err := frl.Take(ctx, "foo")
+		require.Error(t, err)
+		require.False(t, ok)
+	}
+}
