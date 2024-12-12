f34206cc000f25ef2c2d0d4eaa5b3ec8f0539c70
Vui-Chee <46051576+Vui-Chee@users.noreply.github.com>
2024-12-13 07:30:20 +0800

    proxyd: add Redis cluster support (#100)
    feat: add redis cluster config option. update redis.Client to redis.UniversalClient

--- proxyd/redis.go
+++ proxyd/redis.go
@@ -4,19 +4,29 @@ import (
 	"context"
 	"time"
 
+	"github.com/ethereum/go-ethereum/log"
 	"github.com/redis/go-redis/v9"
 )
 
-func NewRedisClient(url string) (*redis.Client, error) {
-	opts, err := redis.ParseURL(url)
-	if err != nil {
-		return nil, err
+func NewRedisClient(url string, enable_cluster bool) (redis.UniversalClient, error) {
+	if enable_cluster {
+		log.Info("Using cluster redis client.")
+		opts, err := redis.ParseClusterURL(url)
+		if err != nil {
+			return nil, err
+		}
+		return redis.NewClusterClient(opts), nil
+	} else {
+		log.Info("Using default redis client.")
+		opts, err := redis.ParseURL(url)
+		if err != nil {
+			return nil, err
+		}
+		return redis.NewClient(opts), nil
 	}
-	client := redis.NewClient(opts)
-	return client, nil
 }
 
-func CheckRedisConnection(client *redis.Client) error {
+func CheckRedisConnection(client redis.UniversalClient) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	if err := client.Ping(ctx).Err(); err != nil {
