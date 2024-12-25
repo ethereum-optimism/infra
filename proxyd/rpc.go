7e5a2e7674b8dcf9af159a9ce71636bd1b3aff37
Michael de Hoog <michael.dehoog@coinbase.com>
2024-12-25 10:12:47 -1000

    Implement range parsing

--- proxyd/rpc.go
+++ proxyd/rpc.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"io"
 	"strings"
+
+	"github.com/ethereum/go-ethereum/common/hexutil"
 )
 
 type RPCReq struct {
@@ -181,6 +183,51 @@ type Range struct {
 }
 
 func ParseRange(req *RPCReq, tracker BlockNumberTracker) *Range {
-	// TODO
-	return nil
+	switch req.Method {
+	case "eth_getLogs", "eth_newFilter":
+		var p []map[string]interface{}
+		if err := json.Unmarshal(req.Params, &p); err != nil {
+			return nil
+		}
+		fromBlock, hasFrom := p[0]["fromBlock"].(string)
+		toBlock, hasTo := p[0]["toBlock"].(string)
+		if !hasFrom && !hasTo {
+			return nil
+		}
+		// if either fromBlock or toBlock is defined, default the other to "latest" if unset
+		if hasFrom && !hasTo {
+			toBlock = "latest"
+		} else if hasTo && !hasFrom {
+			fromBlock = "latest"
+		}
+		from, fromOk := stringToBlockNumber(fromBlock, tracker)
+		to, toOk := stringToBlockNumber(toBlock, tracker)
+		if !fromOk || !toOk {
+			return nil
+		}
+		return &Range{
+			FromBlock: from,
+			ToBlock:   to,
+		}
+	default:
+		return nil
+	}
+}
+
+func stringToBlockNumber(tag string, tracker BlockNumberTracker) (uint64, bool) {
+	switch tag {
+	case "latest":
+		return tracker.GetLatestBlockNumber()
+	case "safe":
+		return tracker.GetSafeBlockNumber()
+	case "finalized":
+		return tracker.GetFinalizedBlockNumber()
+	case "earliest":
+		return 0, true
+	case "pending":
+		latest, ok := tracker.GetLatestBlockNumber()
+		return latest + 1, ok
+	}
+	d, err := hexutil.DecodeUint64(tag)
+	return d, err == nil
 }
