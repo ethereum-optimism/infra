e10211331a1114607227ea0acdc40ed7f5568b81
Stefano Charissis <stefano@oplabs.co>
2025-03-14 07:22:40 +1100

    feat(op-acceptor): handle package tests better. (#225)
    Show the result of each sub-test within a package in the output. Also show the package name.

--- op-acceptor/types/test.go
+++ op-acceptor/types/test.go
@@ -1,6 +1,9 @@
 package types
 
-import "time"
+import (
+	"strings"
+	"time"
+)
 
 // TestStatus represents the possible states of a test execution
 type TestStatus string
@@ -15,8 +18,9 @@ const (
 type TestResult struct {
 	Metadata ValidatorMetadata
 	Status   TestStatus
-	Error    error         // Changed from string to error
-	Duration time.Duration // Track test execution time
+	Error    error                  // Changed from string to error
+	Duration time.Duration          // Track test execution time
+	SubTests map[string]*TestResult // Store individual test results when running a package
 }
 
 // TestConfig represents a test configuration
@@ -24,4 +28,20 @@ type TestConfig struct {
 	Name    string `yaml:"name,omitempty"`
 	Package string `yaml:"package"`
 	RunAll  bool   `yaml:"run_all,omitempty"`
+}
+
+// GetTestDisplayName returns a formatted display name for a test based on its name and metadata
+// If the test name is empty but a package is specified, it will return the package name in a readable format
+func GetTestDisplayName(testName string, metadata ValidatorMetadata) string {
+	displayName := testName
+	if displayName == "" && metadata.Package != "" {
+		// For package names, use a shortened version to avoid wrapping
+		pkgParts := strings.Split(metadata.Package, "/")
+		if len(pkgParts) > 0 {
+			displayName = pkgParts[len(pkgParts)-1] + " (package)"
+		} else {
+			displayName = metadata.Package
+		}
+	}
+	return displayName
 }
