bde511fe11ab3c94b6f7915e17b5ff8b0899a7b2
Jacob Elias <jacob@oplabs.co>
2025-04-24 10:17:15 -0400

    feat: adjusted debug logs

--- op-conductor-ops/op-conductor-ops.py
+++ op-conductor-ops/op-conductor-ops.py
@@ -139,15 +139,18 @@ def status(network: str):
         ]
         # Add rollup boost data if the column exists
         if has_rollup_boost and sequencer.builder_unsafe_l2_number:
-            logging.debug(f"sequencer.l2_unsafe_number: {sequencer.unsafe_l2_number}")
             logging.debug(
-                f"sequencer.builder_l2_unsafe_number: {sequencer.builder_unsafe_l2_number}"
+                f"{sequencer.sequencer_id}.l2_unsafe_number: {sequencer.unsafe_l2_number}"
+            )
+            logging.debug(
+                f"{sequencer.sequencer_id}.builder_l2_unsafe_number: {sequencer.builder_unsafe_l2_number}"
             )
             builder_diff = (
                 2
                 > (sequencer.unsafe_l2_number - sequencer.builder_unsafe_l2_number)
                 > -1
             )
+            logging.debug(f"builder diff: {builder_diff}")
             row_data.append(print_boolean(builder_diff))
 
         table.add_row(*row_data)  # Unpack row data
